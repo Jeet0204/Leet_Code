7f1d84e5fa7e95a755716c669b2d3f35e44ed80f
Jeet0204 <jeetjivrajani@gmail.com>
2020-03-14 16:42:12 -0400

    Same as Easy 118,Just return the last sublist

---
+++ Easy_119_Pascal's_Triangle_II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<=rowIndex;i++)
+        {
+            List<Integer> sublist = new ArrayList<>();
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0 || j==i)
+                    sublist.add(1);
+                else
+                    sublist.add(list.get(i-1).get(j-1)+list.get(i-1).get(j));
+            }
+            list.add(sublist);
+        }
+        return list.get(rowIndex);
+    }
+}
