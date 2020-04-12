63dfe1f6f2cdedd176e98fcbc972ffe504fe5e63
Jeet0204 <jeetjivrajani@gmail.com>
2020-04-12 03:43:28 -0400

    Brute Force: Iterate to the loop and check for the substring, add to the list and also check for the string if it contains in list or not

---
+++ Easy_1408_String_Matching_in_an_Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=0;j<words.length;j++)
+            {
+                if(words[i].contains(words[j]) && i!=j && !list.contains(words[j]))
+                    list.add(words[j]);
+            }
+        }
+        return list;
+    }
+}
